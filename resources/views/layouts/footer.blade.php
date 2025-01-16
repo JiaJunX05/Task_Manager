4fb1035f9b6522b44aa4bc07eceec99ea2718ef2
JiaJunX05 <ohjj2005@gmail.com>
2025-01-16 22:32:59 +0800

    Change Title Footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="bg-body-tertiary text-center">
         <div class="container-fluid p-3 bg-body-tertiary">
             <p class="mb-0">
-                &copy; 2025 Task Manager. All rights reserved. 
+                &copy; 2025 Task Management. All rights reserved. 
                 <span class="text-primary me-2">Terms of Service</span> | 
                 <span class="text-primary ms-2">Privacy Policy</span>
             </p>
