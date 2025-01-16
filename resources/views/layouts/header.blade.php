509b9bf068e9c889b5f5d312eba8c37f2998ef06
JiaJunX05 <ohjj2005@gmail.com>
2025-01-16 22:04:56 +0800

    Change Title Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <!-- Header -->
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('dashboard') }}"><i class="bi bi-house-door-fill me-2"></i>Task Manager</a>
+        <a class="navbar-brand" href="{{ route('dashboard') }}"><i class="bi bi-house-door-fill me-2"></i>Task Management</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
